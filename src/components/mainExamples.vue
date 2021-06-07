2036d40e3234802310eec9f770fd90348d08792c
YangLuLu107 <973853942@qq.com>
2021-06-07 23:59:29 +0800

    内容列表开发

--- src/components/mainExamples.vue
+++ src/components/mainExamples.vue
@@ -26,15 +26,45 @@
     <el-row class="content">
       <el-row>
         <el-row>
-          <h3 id="qcharts-type-line" class="title">折线图</h3>
+          <h3 id="qcharts-type-line" class="content-title">折线图</h3>
         </el-row>
-        <el-row>
-          <el-col :span="6">1</el-col>
-          <el-col :span="6">2</el-col>
-          <el-col :span="6">3</el-col>
-          <el-col :span="6">4</el-col>
-          <el-col :span="6">5</el-col>
-          <el-col :span="6">6</el-col>
+        <el-row class="sub-content">
+          <el-col :span="6">
+            <a target="_blank" href="#" class="detail-link">
+              <el-row><img src="../assets/images/line-simple.webp" class="thumbnail"></el-row>
+              <el-row class="sub-title"><span>基础折线图</span></el-row>
+            </a>
+          </el-col>
+          <el-col :span="6">
+            <a target="_blank" href="#" class="detail-link">
+              <el-row><img src="../assets/images/line-smooth.webp" class="thumbnail"></el-row>
+              <el-row class="sub-title"><span>基础平滑折线图</span></el-row>
+            </a>
+          </el-col>
+          <el-col :span="6">
+            <a target="_blank" href="#" class="detail-link">
+              <el-row><img src="../assets/images/area-basic.webp" class="thumbnail"></el-row>
+              <el-row class="sub-title"><span>基础面积图</span></el-row>
+            </a>
+          </el-col>
+          <el-col :span="6">
+            <a target="_blank" href="#" class="detail-link">
+              <el-row><img src="../assets/images/line-stack.webp" class="thumbnail"></el-row>
+              <el-row class="sub-title"><span>折线图堆叠</span></el-row>
+            </a>
+          </el-col>
+          <el-col :span="6">
+            <a target="_blank" href="#" class="detail-link">
+              <el-row><img src="../assets/images/area-stack.webp" class="thumbnail"></el-row>
+              <el-row class="sub-title"><span>基堆叠面积图</span></el-row>
+            </a>
+          </el-col>
+          <el-col :span="6">
+            <a target="_blank" href="#" class="detail-link">
+              <el-row><img src="../assets/images/area-stack-gradient.webp" class="thumbnail"></el-row>
+              <el-row class="sub-title"><span>渐变堆叠面积图</span></el-row>
+            </a>
+          </el-col>
         </el-row>
       </el-row>
     </el-row>
@@ -74,10 +104,30 @@
     flex: 1;
     padding: 0 2rem;
   }
-  .title {
+  .content-title {
     border-bottom: 1px solid #E1E5F2;
     padding: 8px 0;
     font-family: 'Alibaba-PuHuiTi-Medium';
     font-weight: 400;
+  }
+  .thumbnail {
+    width: 95%;
+    height: 95%;
+    border-radius: 0.25rem;
+    box-shadow: 0 0 20px rgb(0 0 0 / 5%);
+    margin-top: 1rem;
+  }
+  .sub-content .el-col {
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+    text-align: center;
+    transition: all 0.5s;
+    transform: scale(1);
+  }
+  .sub-content .el-col:hover {
+    transform: scale(1.08);
+  }
+  .sub-title {
+    
   }
 </style>
